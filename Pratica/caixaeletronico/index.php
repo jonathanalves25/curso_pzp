f55f02dd1423039154e15459dbe5ed3cef481020
Jonathan Alves <johnnyduff25@gmail.com>
2020-05-21 04:06:39 -0300

    caixa eletronico

---
+++ Pratica/caixaeletronico/index.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require 'config.php';
+if(isset($_SESSION['banco']) && !empty($_SESSION['banco'])) {
+    $id = $_SESSION['banco'];
+
+    $sql = $pdo->prepare("SELECT * FROM contas WHERE id = :id");
+    $sql->bindValue(":id", $id);
+    $sql->execute();
+
+    if ($sql->rowCount() > 0 ) {
+        $info = $sql->fetch();
+    } else {
+        header("Location: login.php");
+        exit;
+    }
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<html>
+<title>
+    Caixa Eletrônico
+</title>
+<body>
+    <h1>Banco Ximbicudos</h1>
+    Titular: <?php echo $info['titular']; ?>
+    Agência: <?php echo $info['agencia']; ?><br>
+    Conta: <?php echo $info['conta']; ?><br>
+    Saldo: <?php echo $info['saldo']; ?><br>
+    <a href="">Sair</a>
+</body>
+</html>
---
+++ Pratica/caixaeletronico/login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+require 'config.php';
+
+if (isset($_POST['agencia']) && !empty($_POST['agencia'])) {
+    $agencia = addslashes($_POST['agencia']);
+    $conta = addslashes($_POST['conta']);
+    $senha = addslashes($_POST['senha']);
+
+    $sql = $pdo->prepare("SELECT * FROM contas WHERE agencia = :agencia AND conta = :oonta AND senha = :senha");
+    $sql->bindValue(":agencia", $agencia);
+    $sql->bindValue(":conta", $conta);
+    $sql->bindValue(":senha", md5($senha));
+    $sql->execute();
+
+    if($sql->rowCount() > 0) {
+        $sql = $sql->fetch();
+        $_SESSION['banco'] = $sql['id'];
+        header("Location: index.php");
+        exit;
+    }
+}
+?>
+
+<html>
+<title>
+    Caixa Eletrônico
+</title>
+<body>
+<form method="post">
+    Agência:<br>
+    <input type="text" name="agencia"><br>
+    Conta:<br>
+    <input type="number" name="conta"><br>
+    Senha:<br>
+    <input type="password" name="senha"><br><br>
+    <input type="submit" value="Entrar">
+</form>
+</body>
+</html>
