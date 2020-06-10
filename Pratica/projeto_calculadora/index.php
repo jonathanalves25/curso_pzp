14dc006ccfed37d13855a2e869ab71336f860ee9
Jonathan Alves <johnnyduff25@gmail.com>
2020-06-10 03:46:02 -0300

    projeto calculadora simples

---
+++ Pratica/projeto_calculadora/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<title>Calculadora</title>
+<body>
+    <form method="get" action="calculo.php">
+        <input type="text" name="n1">
+
+        <select name="op">
+            <option> - </option>   
+            <option> + </option>   
+            <option> / </option>   
+            <option> * </option>     
+        </select>
+
+        <input type="text" name="n2">
+
+        <input type="submit" value="Calcular">
+    </form>
+</body>
+</html>
